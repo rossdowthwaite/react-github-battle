9a096d985267a24234cc27a88627969520c81fb2
Ross Dowthwaite <ross.dowthwaite@theunit.co.uk>
2017-12-07 15:13:19 +0000

    axios, promises and Github API

--- app/components/results.js
+++ app/components/results.js
@@ -1,5 +1,9 @@
 var React = require('react');
 
+// var id = "YOUR_CLIENT_ID";
+// var sec = "YOUR_SECRET_ID";
+// var params = "?client_id=" + id + "&client_secret=" + sec;
+
 class Results extends React.Component {
   render() {
     return (
