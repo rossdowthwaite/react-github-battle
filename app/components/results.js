8d8ed228bb2a5515152e97a4c2513a040148906a
Ross Dowthwaite <ross.dowthwaite@theunit.co.uk>
2017-12-07 15:17:32 +0000

    ghdfhfg

--- app/components/results.js
+++ app/components/results.js
@@ -1,11 +1,7 @@
 var React = require('react');
 
-// var id = "YOUR_CLIENT_ID";
-// var sec = "YOUR_SECRET_ID";
-// var params = "?client_id=" + id + "&client_secret=" + sec;
-
 class Results extends React.Component {
-  render() {
+  render() { d
     return (
       <div>Results</div>
     )
--- app/utils/api.js
+++ app/utils/api.js
@@ -62,7 +62,7 @@ module.exports = {
     return axios.all(players.map(getUserData))
       .then(sortPlayers)
       .catch(handleError)
-  }
+  },
 
   fetchPopularRepos: function (language) {
     var encodedURI = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:'+ language + '&sort=stars&order=desc&type=Repositories');
